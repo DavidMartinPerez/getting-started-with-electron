2cbe17726cfe254c288dc94026998b1c397db442
David Martín Pérez <davidmartinperez1@gmail.com>
2019-03-28 19:57:56 +0100

    añadido tamaño de ventana

--- video-info/index.js
+++ video-info/index.js
@@ -9,13 +9,10 @@ app.on('ready', () => {
     console.log('App is now ready');
 
     mainWindow = new BrowserWindow({
-        width: 360,
+        width: 840,
         height: 680
     });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-    console.log(ipcMain)
-
     // ######3######################## Comunicación con electron -> webview ########################################
     // MainWindow (ipcRenderer.send) -- Webview envia evento a electron -> Electron App (ipcMain.on)
     // Electron App (mainWindow.webContents.send) --  electron envia evento al webview-> MainWindow (ipcRenderer.on)
