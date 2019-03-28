36991c5cf5feafeda25ff9a2ada0dfdd144771f5
David Martín Pérez <davidmartinperez1@gmail.com>
2019-03-28 09:22:00 +0100

    Comunicación entre webview y electron

--- video-info/index.js
+++ video-info/index.js
@@ -1,10 +1,22 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     console.log('App is now ready');
+    console.log(electron.ipcRenderer)
 
     const mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // ######3######################## Comunicación con electron -> webview ########################################
+    // MainWindow (ipcRenderer.send) -- Webview envia evento a electron -> Electron App (ipcMain.on)
+    // Electron App (mainWindow.webContents.send) --  electron envia evento al webview-> MainWindow (ipcRenderer.on)
+    // ######3######################## Comunicación con electron -> webview ########################################
+
+
+})
+
+ipcMain.on('video:submit', () => {
+    console.log("Hey")
 })
